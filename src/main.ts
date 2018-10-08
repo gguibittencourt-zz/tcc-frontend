8bf370aa49d431860f96c1023d42e1f0aece2898
Guilherme Silva <guilherme.silva@digilab.com.br>
2018-10-08 18:16:59 -0300

    bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 ﻿import './polyfills';
+import './styles/styles.scss';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,101 @@
 ﻿const webpack = require('webpack');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: ['ts-loader', 'angular2-template-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(html|css)$/,
-        loader: 'raw-loader'
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    }),
-    new webpack.DefinePlugin({
-      // global app config object
-      config: JSON.stringify({
-        apiUrl: 'http://localhost:4000'
-      })
-    })
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-    runtimeChunk: true
-  },
-  devServer: {
-    historyApiFallback: true
-  }
+	entry: [
+		'./src/main.ts'
+	],
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: ['ts-loader', 'angular2-template-loader'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-loader'
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: "style-loader"
+				}, {
+					loader: "css-loader", options: {
+						sourceMap: true
+					}
+				}, {
+					loader: "sass-loader", options: {
+						sourceMap: true
+					}
+				}]
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'url-loader?limit=10000',
+			}, {
+				test: /\.(eot|ttf|wav|mp3|pdf)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'file-loader',
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.scss']
+	},
+	plugins: [
+		new CleanWebpackPlugin('build'),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+			inject: 'body'
+		}),
+		new webpack.DefinePlugin({
+			// global app config object
+			config: JSON.stringify({
+				apiUrl: 'http://localhost:4000'
+			})
+		}),
+		new webpack.ProvidePlugin({
+			jQuery: 'jquery',
+			$: 'jquery',
+			jquery: 'jquery'
+		}),
+		new ExtractTextWebpackPlugin('styles/styles.scss'),
+		new OptimizeCssAssetsWebpackPlugin()
+	],
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					chunks: "initial",
+					minChunks: 3,
+					name: "commons",
+					enforce: true
+				}
+			}
+		},
+		runtimeChunk: true,
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				uglifyOptions: {
+					compress: false,
+					ecma: 6,
+					mangle: true
+				},
+				sourceMap: true
+			})
+		]
+	},
+	devServer: {
+		port: 3000,
+		contentBase: './src/',
+		historyApiFallback: true
+	}
 };
