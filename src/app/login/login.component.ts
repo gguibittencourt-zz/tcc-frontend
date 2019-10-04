f75657bd1169cd4494d5932af94dc13eb7d13c62
Guilherme Bittencourt da Silva <gguibittencourt@gmail.com>
2019-10-04 00:13:37 -0300

    wip

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,50 +6,56 @@ import {AuthenticationService} from '../_services';
 import {SnackBarComponent} from "../_directives/snack-bar";
 import {MatSnackBar} from "@angular/material";
 
-@Component({templateUrl: 'login.component.html'})
+@Component({
+	templateUrl: 'login.component.html',
+	styleUrls: ['login.component.scss']
+})
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
+	loginForm: FormGroup;
+	loading = false;
+	submitted = false;
+	returnUrl: string;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService,
-		private snackBar: MatSnackBar) {}
+	constructor(
+		private formBuilder: FormBuilder,
+		private route: ActivatedRoute,
+		private router: Router,
+		private authenticationService: AuthenticationService,
+		private snackBar: MatSnackBar) {
+	}
 
-    ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
+	ngOnInit() {
+		this.loginForm = this.formBuilder.group({
+			username: ['', Validators.required],
+			password: ['', Validators.required]
+		});
 
-        this.authenticationService.logout();
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
+		this.authenticationService.logout();
+		this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+	}
 
-    get f() { return this.loginForm.controls; }
+	get f() {
+		return this.loginForm.controls;
+	}
 
-    onSubmit() {
-        this.submitted = true;
+	onSubmit() {
+		this.submitted = true;
 
-        if (this.loginForm.invalid) {
-            return;
-        }
+		if (this.loginForm.invalid) {
+			return;
+		}
 
-        this.loading = true;
-        this.authenticationService.login(this.f.username.value, this.f.password.value)
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                	this.createSnackBar(error, 'error');
-                    this.loading = false;
-                });
-    }
+		this.loading = true;
+		this.authenticationService.login(this.f.username.value, this.f.password.value)
+			.subscribe(
+				data => {
+					this.router.navigate([this.returnUrl]);
+				},
+				error => {
+					this.createSnackBar(error, 'error');
+					this.loading = false;
+				});
+	}
 
 	private createSnackBar(message: string, panelClass: string): void {
 		this.snackBar.openFromComponent(SnackBarComponent, {
